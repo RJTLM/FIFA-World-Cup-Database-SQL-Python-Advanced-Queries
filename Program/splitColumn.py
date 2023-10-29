33345eadc1bda38beb6d5ba5ae7d386b87948718
Ryan Mackintosh <ryan.mackintosh@hotmail.com>
2023-10-29 10:40:25 +0800

    Bug fixing

--- Program/splitColumn.py
+++ Program/splitColumn.py
@@ -8,17 +8,54 @@ def clean_data(data):
 def extract_columns(data, column_names):
     """Extract specific columns from the data."""
     header = data[0]
-    missing_columns = [name for name in column_names if name not in header]
-    if missing_columns:
-        print(f"Columns {', '.join(missing_columns)} not found in the data.")
-        return None
-    
-    column_indices = [header.index(name) for name in column_names]
+    column_indices = [header.index(name) if name in header else None for name in column_names]
     extracted_data = [column_names]
     for row in data[1:]:
-        extracted_data.append([row[index] for index in column_indices])
-    
+        extracted_row = [row[index] if index is not None else '' for index in column_indices]
+        extracted_data.append(extracted_row)
     return extracted_data
+
+def update_csv_file(filename, new_data):
+    """Update the CSV file with new data. Add new columns or update existing ones."""
+    if not os.path.exists(filename):
+        with open(filename, 'w', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerows(new_data)
+        return
+    
+    with open(filename, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        existing_data = [row for row in reader]
+    
+    existing_header = existing_data[0]
+    new_header = new_data[0]
+    
+    # Mapping of column name to its index in existing data
+    existing_columns = {name: index for index, name in enumerate(existing_header)}
+    
+    # Add new columns to existing data
+    for column in new_header:
+        if column not in existing_columns:
+            existing_columns[column] = len(existing_header)
+            existing_header.append(column)
+            for row in existing_data[1:]:
+                row.append('')
+    
+    # Update existing data with new data
+    for i, new_row in enumerate(new_data[1:], start=1):
+        if i < len(existing_data):
+            for column, new_value in zip(new_header, new_row):
+                existing_data[i][existing_columns[column]] = new_value
+        else:
+            # If new data has more rows than existing data, add new rows
+            new_row_extended = [''] * len(existing_header)
+            for column, new_value in zip(new_header, new_row):
+                new_row_extended[existing_columns[column]] = new_value
+            existing_data.append(new_row_extended)
+    
+    with open(filename, 'w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerows(existing_data)
 
 if __name__ == "__main__":
     input_filename = input("Enter the name of the CSV file to process: ")
@@ -40,7 +77,7 @@ if __name__ == "__main__":
 
     while True:
         # Give user the option to exit or extract data
-        choice = input("Choose an option:\n1. Exit\n2. Extract Data to a New CSV File\n3. Append Data to an Existing CSV File\n")
+        choice = input("Choose an option:\n1. Exit\n2. Extract Data to a New CSV File\n3. Update or Add Columns in an Existing CSV File\n")
         if choice == "1":
             break
         elif choice in ["2", "3"]:
@@ -51,18 +88,14 @@ if __name__ == "__main__":
             if cleaned_data is not None:
                 if choice == "2":
                     output_filename = input("Enter the name for the new CSV file: ")
-                    mode = 'w'
+                    with open(output_filename, 'w', newline='', encoding='utf-8') as outfile:
+                        writer = csv.writer(outfile)
+                        writer.writerows(cleaned_data)
+                    print(f"Data saved to {output_filename}")
                 else:
-                    output_filename = input("Enter the name of the existing CSV file to append to: ")
-                    mode = 'a' if os.path.exists(output_filename) else 'w'
-                
-                with open(output_filename, mode, newline='', encoding='utf-8') as outfile:
-                    writer = csv.writer(outfile)
-                    if mode == 'w' or os.path.getsize(output_filename) == 0:
-                        writer.writerow(cleaned_data[0])  # Write header if file is empty
-                    writer.writerows(cleaned_data[1:])
-                
-                print(f"Data saved to {output_filename}")
+                    output_filename = input("Enter the name of the existing CSV file to update or add columns: ")
+                    update_csv_file(output_filename, cleaned_data)
+                    print(f"Data updated in {output_filename}")
             else:
                 print("Operation aborted.")
         else:
