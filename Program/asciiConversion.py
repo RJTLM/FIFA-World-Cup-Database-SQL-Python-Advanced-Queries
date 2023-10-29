10ae070389841d2ebee3bb4b4b9218266ad6e4a1
Ryan Mackintosh <ryan.mackintosh@hotmail.com>
2023-10-29 15:13:27 +0800

    bug fix for user input

--- Program/asciiConversion.py
+++ Program/asciiConversion.py
@@ -1,9 +1,10 @@
 import csv
-import unidecode # Ensure you have the unidecode library installed. You can install it using pip: pip install unidecode
+import unidecode  # Ensure you have the unidecode library installed. You can install it using pip: pip install unidecode
 
 # Reference: Unidecode on PyPI
 # https://pypi.org/project/Unidecode/
 # Description: This is the official Python Package Index (PyPI) page for the Unidecode library.
+
 def clean_non_ascii(text):
     """Convert non-ASCII characters to their closest ASCII equivalents."""
     return unidecode.unidecode(text)
@@ -11,13 +12,17 @@ def clean_non_ascii(text):
 
 def clean_csv(input_file, output_file):
     """Read a CSV file, clean non-ASCII characters, and write to a new file."""
-    with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', newline='', encoding='utf-8') as outfile:
-        reader = csv.reader(infile)
-        writer = csv.writer(outfile)
+    try:
+        with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', newline='', encoding='utf-8') as outfile:
+            reader = csv.reader(infile)
+            writer = csv.writer(outfile)
 
-        for row in reader:
-            cleaned_row = [clean_non_ascii(cell) for cell in row]
-            writer.writerow(cleaned_row)
+            for row in reader:
+                cleaned_row = [clean_non_ascii(cell) for cell in row]
+                writer.writerow(cleaned_row)
+        print(f"Cleaned CSV saved to {output_file}")
+    except FileNotFoundError:
+        print(f"The file {input_file} was not found. Please make sure the file exists and try again.")
     # Reference: "FOP Sem2 2023 Lecture Material" (for understanding file I/O and CSV file manipulation in Python)
 
 def main():
@@ -32,12 +37,15 @@ def main():
             output_filename = input("Enter the name for the cleaned CSV file: ")
             
             clean_csv(input_filename, output_filename)
-            print(f"Cleaned CSV saved to {output_filename}")
         elif choice == "0":
             print("Returning to Main Menu...")
             break
         else:
             print("Invalid option. Please choose a valid option.")
+    # Reference: "FOP Sem2 2023 Lecture Material" (for understanding the main function and script execution in Python)
+
+if __name__ == "__main__":
+    main()
     # Reference: "FOP Sem2 2023 Lecture Material" (for understanding the main function and script execution in Python)
 
 # Reference: How to use unidecode in python (3.3) - Stack Overflow
