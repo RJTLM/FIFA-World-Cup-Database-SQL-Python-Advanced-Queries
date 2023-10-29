5b625a8b0d3dea3b5f4c9a103c36c89e0180daed
Ryan Mackintosh <ryan.mackintosh@hotmail.com>
2023-10-29 11:17:19 +0800

    Bug fixes

--- Program/splitColumn.py
+++ Program/splitColumn.py
@@ -4,18 +4,41 @@ def split_officials_column(input_file, output_file):
     # Read the CSV file
     df = pd.read_csv(input_file)
     
-    # Split the 'Officials' column into four new columns
-    df[['Referee1', 'Referee2', 'Referee3', 'Referee4']] = df['Officials'].str.split(' \* ', expand=True)
+    # Print the column names to verify they are being read correctly
+    print(df.columns)
     
-    # Move the 'Referee' column to column "E"
-    referee_col = df.pop('Referee')
-    df.insert(4, 'Referee', referee_col)
+    # Check if 'Officials' column is in the dataframe
+    if 'Officials' not in df.columns:
+        print("Error: 'Officials' column not found in the input file")
+        return
     
-    # Save the updated DataFrame to a new CSV file
+    # Split the 'Officials' column into a new dataframe
+    officials_split = df['Officials'].str.split(' \* ', expand=True)
+    
+    # Print the shape of officials_split to debug the issue
+    print("Shape of officials_split:", officials_split.shape)
+    
+    # Get the number of referees (columns after split)
+    num_referees = officials_split.shape[1]
+    
+    # Create dynamic column names based on the number of referees
+    new_column_names = [f'Referee{i+1}' for i in range(num_referees)]
+    
+    # Rename the new columns
+    officials_split.columns = new_column_names
+    
+    # Concatenate the new columns to the original dataframe
+    df = pd.concat([df, officials_split], axis=1)
+    
+    # Drop the original 'Officials' column
+    df = df.drop(columns=['Officials'])
+    
+    # Write the updated dataframe to a new CSV file
     df.to_csv(output_file, index=False)
-    print(f"Updated CSV file saved to {output_file}")
 
-if __name__ == "__main__":
-    input_file = 'Officials.csv'
-    output_file = 'UpdatedOfficials.csv'
-    split_officials_column(input_file, output_file)
+# File paths
+input_file = 'Officials.csv'
+output_file = 'UpdatedOfficials.csv'
+
+# Run the function
+split_officials_column(input_file, output_file)
