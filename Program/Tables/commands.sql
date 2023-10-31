40af8d37d71a9d57b4f9b1834880ab016247b93b
Ryan Mackintosh <ryan.mackintosh@hotmail.com>
2023-10-31 18:26:20 +0800

    reorder commands.sql sourcing to create tables without FK dependencies second

--- Program/Tables/commands.sql
+++ Program/Tables/commands.sql
@@ -6,21 +6,23 @@
 -- Create and Use fifa_womens_world_cup Database
 source ./Program/Tables/CreateTables/createDatabase.sql;
 
--- Create Entity Sets Tables
-source ./Program/Tables/CreateTables/createCard.sql;
-source ./Program/Tables/CreateTables/createCoach.sql;
+-- Create tables that do not have foreign key dependencies (figured this out from trial and error)
 source ./Program/Tables/CreateTables/createCountry.sql;
-source ./Program/Tables/CreateTables/createEvent.sql;
-source ./Program/Tables/CreateTables/createGoal.sql;
-source ./Program/Tables/CreateTables/createFootballMatch.sql;
-source ./Program/Tables/CreateTables/createOfficial.sql;
-source ./Program/Tables/CreateTables/createOfficialRole.sql;
+source ./Program/Tables/CreateTables/createCoach.sql;
 source ./Program/Tables/CreateTables/createPlayer.sql;
-source ./Program/Tables/CreateTables/createStage.sql;
-source ./Program/Tables/CreateTables/createSubstitution.sql;
 source ./Program/Tables/CreateTables/createTeam.sql;
 source ./Program/Tables/CreateTables/createVenue.sql;
+source ./Program/Tables/CreateTables/createOfficialRole.sql;
+source ./Program/Tables/CreateTables/createOfficial.sql;
+source ./Program/Tables/CreateTables/createStage.sql;
+
+-- Create tables that have foreign key dependencies
+source ./Program/Tables/CreateTables/createEvent.sql;
+source ./Program/Tables/CreateTables/createFootballMatch.sql;
+source ./Program/Tables/CreateTables/createCard.sql;
+source ./Program/Tables/CreateTables/createGoal.sql;
+source ./Program/Tables/CreateTables/createSubstitution.sql;
 source ./Program/Tables/CreateTables/createWinner.sql;
 
--- Create Relationship Sets Tables
+-- Create the relationship sets tables
 source ./Program/Tables/CreateTables/createRelationshipSets.sql;
