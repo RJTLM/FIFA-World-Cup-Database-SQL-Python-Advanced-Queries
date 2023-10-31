28fb4cf00cff62ff9e00d613dce468a4de924454
Ryan Mackintosh <ryan.mackintosh@hotmail.com>
2023-10-31 23:29:15 +0800

    update comment

--- Program/Tables/CreateTables/createPlayer.sql
+++ Program/Tables/CreateTables/createPlayer.sql
@@ -8,6 +8,6 @@ CREATE TABLE IF NOT EXISTS Player (
   FOREIGN KEY (TeamName) REFERENCES Team(TeamName) ON DELETE CASCADE
 );
 
--- Add foreign key now both tables have been created
+-- Add foreign key now both Team and Player tables have been created
 ALTER TABLE Team
 ADD FOREIGN KEY (Captain) REFERENCES Player(PlayerName) ON DELETE SET NULL;
