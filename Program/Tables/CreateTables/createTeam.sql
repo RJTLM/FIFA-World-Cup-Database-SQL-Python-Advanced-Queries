34b9c42fc8d02949ac2e3cb8a56aaa9dad56ebeb
Ryan Mackintosh <ryan.mackintosh@hotmail.com>
2023-10-31 08:55:35 +0800

    add UNIQUE constraint to manager and captain columns to match updated schema

--- Program/Tables/CreateTables/createTeam.sql
+++ Program/Tables/CreateTables/createTeam.sql
@@ -4,8 +4,8 @@
 -- Create Team table with constraints
 CREATE TABLE IF NOT EXISTS Team (
   TeamName VARCHAR(255) PRIMARY KEY,
-  Manager VARCHAR(255),
-  Captain VARCHAR(255),
+  Manager VARCHAR(255) UNIQUE,
+  Captain VARCHAR(255) UNIQUE,
   FOREIGN KEY (Manager) REFERENCES Coach(CoachName) ON DELETE SET NULL,
   FOREIGN KEY (Captain) REFERENCES Player(PlayerName) ON DELETE SET NULL
 );
