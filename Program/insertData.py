671fb963764a79f82f84a3186a3fea0fcf1fc95c
Ryan Mackintosh <ryan.mackintosh@hotmail.com>
2023-11-01 13:37:29 +0800

    UX update

--- Program/insertData.py
+++ Program/insertData.py
@@ -1,11 +1,15 @@
 import csv
 
 def insert_into_country(cursor, db_connection, data):
-    print(f"Inserting {data} into Country table...")
+    if not data:
+        print("No data to insert into Country table.")
+        return
+    
+    print("Inserting data into Country table...")
     sql = "INSERT IGNORE INTO Country (CountryName) VALUES (%s)"
-    cursor.execute(sql, (data,))
+    cursor.executemany(sql, [(country,) for country in data])
     db_connection.commit()
-    print(f"{data} inserted successfully.")
+    print("Country table data insertion complete. {} rows processed.".format(len(data)))
 
 def insert_data(cursor, db_connection):
     print("Starting data insertion process...")
@@ -17,15 +21,22 @@ def insert_data(cursor, db_connection):
         # Read data from CSV file and insert into tables
         with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
             csv_reader = csv.DictReader(csvfile)
+            country_data = []
             row_count = 0
             for row in csv_reader:
-                # Insert data into Country table
+                # Collect data for Country table
                 # Assuming 'EventHost' column in CSV corresponds to 'CountryName' in Country table
-                insert_into_country(cursor, db_connection, row['EventHost'])
+                country_data.append(row['EventHost'])
                 row_count += 1
-                # Add similar code here to insert data into other tables
+                # Collect similar data here for other tables
                 # ...
-            print(f"Data insertion complete. {row_count} rows processed.")
+
+            # Insert data into Country table
+            insert_into_country(cursor, db_connection, country_data)
+            # Insert similar data here for other tables
+            # ...
+
+            print("Data insertion complete. {} rows processed.".format(row_count))
     except Exception as e:
         print("An error occurred during the data insertion process.")
-        print(f"Error: {str(e)}")
+        print("Error: {}".format(str(e)))
