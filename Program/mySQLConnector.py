0757877bade1d7faaad74eb699eefaccd07180a5
Ryan Mackintosh <ryan.mackintosh@hotmail.com>
2023-11-01 11:42:23 +0800

    debug

--- Program/mySQLConnector.py
+++ Program/mySQLConnector.py
@@ -4,10 +4,10 @@ import getpass
 
 # Database configuration
 db_config = {
-    'host': 'your_host',
+    'host': 'localhost',
     'user': input("Enter your MySQL username: "),
     'password': getpass.getpass("Enter your MySQL password: "),
-    'database': 'your_database_name'
+    'database': 'fifa_womens_world_cup_21171466'
 }
 
 # Establish a database connection
@@ -19,7 +19,7 @@ csv_file_path = './Program/bigDataCleaned.csv'
 
 # Function to insert data into Country table
 def insert_into_country(data):
-    sql = "INSERT INTO Country (CountryName) VALUES (%s)"
+    sql = "INSERT IGNORE INTO Country (CountryName) VALUES (%s)"
     cursor.execute(sql, (data,))
     db_connection.commit()
 
@@ -30,7 +30,6 @@ with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
         # Insert data into Country table
         # Assuming 'EventHost' column in CSV corresponds to 'CountryName' in Country table
         insert_into_country(row['EventHost'])
-
         # Add similar code here to insert data into other tables
         # ...
 
