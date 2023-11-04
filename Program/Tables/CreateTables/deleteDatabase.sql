202064b6e6332683d8c2a81a3c9a7bb980067c5e
Ryan Mackintosh <ryan.mackintosh@hotmail.com>
2023-11-04 20:15:01 +0800

    Bug fix

--- Program/Tables/CreateTables/deleteDatabase.sql
+++ Program/Tables/CreateTables/deleteDatabase.sql
@@ -1,5 +1,9 @@
 /* deleteDatabase.sql: MySQL file for database creation*/
 
 -- Created this for better UX when testing, refactoring and debugging so I didn't need to exit program
--- Delete database
+
+-- Delete tables if they exist
+DROP TABLE IF EXISTS BigData, Event, FootballMatch, LittleData, Manager, Manages, Player, Plays, Referee, Team;
+
+-- Delete the database
 DROP DATABASE IF EXISTS fifa_womens_world_cup_21171466;
