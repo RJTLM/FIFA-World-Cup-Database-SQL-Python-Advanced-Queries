ebe0ecbf5486bdf018a78602f884370ce947f8ec
Ryan Mackintosh <ryan.mackintosh@hotmail.com>
2023-11-04 20:25:28 +0800

    reorder drops

--- Program/Tables/CreateTables/deleteDatabase.sql
+++ Program/Tables/CreateTables/deleteDatabase.sql
@@ -3,7 +3,7 @@
 -- Created this for better UX when testing, refactoring and debugging so I didn't need to exit program
 
 -- Delete tables if they exist
-DROP TABLE IF EXISTS BigData, Event, FootballMatch, LittleData, Manager, Manages, Player, Plays, Referee, Team;
+DROP TABLE IF EXISTS Plays, Manages, FootballMatch, Event, Referee, Manager, Player, Team;
 
 -- Delete the database
 DROP DATABASE IF EXISTS fifa_womens_world_cup_21171466;
