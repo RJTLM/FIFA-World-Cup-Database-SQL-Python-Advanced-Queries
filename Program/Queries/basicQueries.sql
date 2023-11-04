e1021e797e4c8ad1900df410b6093d44a8d38413
Ryan Mackintosh <ryan.mackintosh@hotmail.com>
2023-11-04 21:13:17 +0800

    Fix query 1

--- Program/Queries/basicQueries.sql
+++ Program/Queries/basicQueries.sql
@@ -2,7 +2,9 @@
 
 -- Q3. Part3 Level 1 Basic Queries:
 -- Find all matches played by a specific team
-SELECT * FROM FootballMatch WHERE home_team = ? OR away_team = ?;
+SELECT FM.* FROM FootballMatch FM
+JOIN Plays P ON FM.MatchID = P.MatchID
+WHERE P.TeamName = ?;
 
 -- Retrieve all matches with attendance greater than 50,000
 SELECT * FROM FootballMatch WHERE Attendance > 50000;
@@ -14,4 +16,4 @@ SELECT * FROM FootballMatch WHERE MatchDate BETWEEN '2023-08-01' AND '2023-08-31
 SELECT * FROM FootballMatch WHERE home_score > 2;
 
 -- Retrieve all matches refereed by a specific referee
-SELECT * FROM FootballMatch WHERE Referee = 'Tori Penso';
+SELECT * FROM FootballMatch WHERE RefereeName = 'Tori Penso';
