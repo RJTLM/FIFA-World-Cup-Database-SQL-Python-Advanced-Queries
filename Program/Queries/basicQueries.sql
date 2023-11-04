2cd9e86141bdcaeb28886e467e8221e1d4a2daa8
Ryan Mackintosh <ryan.mackintosh@hotmail.com>
2023-11-05 00:22:07 +0800

    undo refactoring

--- Program/Queries/basicQueries.sql
+++ Program/Queries/basicQueries.sql
@@ -2,30 +2,9 @@
 
 -- Q3. Part3 Level 1 Basic Queries:
 -- Find all matches played by a specific team
-/*SELECT FM.* FROM FootballMatch FM
+SELECT FM.* FROM FootballMatch FM
 JOIN Plays P ON FM.MatchID = P.MatchID
-WHERE P.TeamName IN ('Sweden');*/
-SELECT 
-    FM.*,
-    HomeTeam.TeamName AS HomeTeam,
-    AwayTeam.TeamName AS AwayTeam
-FROM 
-    FootballMatch FM
-JOIN 
-    Plays HomePlays ON FM.MatchID = HomePlays.MatchID
-JOIN 
-    Team HomeTeam ON HomePlays.TeamName = HomeTeam.TeamName
-JOIN 
-    Plays AwayPlays ON FM.MatchID = AwayPlays.MatchID
-JOIN 
-    Team AwayTeam ON AwayPlays.TeamName = AwayTeam.TeamName
-WHERE 
-    HomePlays.TeamName = 'Sweden' OR AwayPlays.TeamName = 'Sweden'
-GROUP BY 
-    FM.MatchID
-HAVING 
-    MIN(HomePlays.TeamName) = HomeTeam.TeamName AND MAX(AwayPlays.TeamName) = AwayTeam.TeamName;
-
+WHERE P.TeamName IN ('Sweden');
 
 -- Retrieve all matches with attendance greater than 50,000
 SELECT * FROM FootballMatch WHERE Attendance > 50000;
