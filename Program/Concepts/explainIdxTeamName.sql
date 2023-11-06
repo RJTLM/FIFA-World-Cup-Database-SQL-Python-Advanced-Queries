338e04b0ed67c32ab1a50d261c3311c81e233be5
Ryan Mackintosh <ryan.mackintosh@hotmail.com>
2023-11-07 04:19:23 +0800

    debug

--- Program/Concepts/explainIdxTeamName.sql
+++ Program/Concepts/explainIdxTeamName.sql
@@ -1,2 +1,2 @@
 -- Explain the index usage for TeamName
-EXPLAIN SELECT * FROM Plays USE INDEX (idx_teamname) WHERE TeamName = 'USA';
+EXPLAIN SELECT * FROM Plays USE INDEX (idx_teamname) WHERE TeamName = 'Sweden';
